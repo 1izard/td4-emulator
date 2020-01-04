2fb366745798ab41a4daf151cef18550d554c645
1izard <dee1bdnatiaw@gmail.com>
2020-01-05 03:44:16 +0900

    Ramen time succeeded

--- TD4-emulator.py
+++ TD4-emulator.py
@@ -12,14 +12,14 @@ def main():
 
     q_d = utils.bastr2ba('0000')    # MUX input; cd is fixed with 0000
     q_c_in = utils.bastr2ba('0000')
-    count = 0
+    count = -1
     for ck, reset in CLOCK_GENERATOR:
         q_PC, q_a, q_b, q_c_out, c_flag = PC.send((ck, reset)), REGISTER_A.send((ck, reset)),\
             REGISTER_B.send((ck, reset)), REGISTER_C.send((ck, reset)), D_FF_C.send((ck, reset))
-        print(f'q_c_out: {q_c_out}')
+        print(f'count: {count}, q_c_out: {q_c_out}')
         op_arr = ROM(q_PC)
         select_a, select_b, load0_, load1_, load2_, load3_ = \
-            (bool(b) for b in units.DECODER(op_arr[4:], c_flag))
+            (bool(b) for b in units.DECODER(op_arr[4:], units.NOT(c_flag)))
         selected_arr = units.MUX(select_a, select_b, q_a, q_b, q_c_in, q_d)
         res_arr = units.ALU(c_flag, selected_arr, op_arr[:4])
         c, sum_arr = bool(res_arr[0]), res_arr[1:]
@@ -28,7 +28,7 @@ def main():
         REGISTER_B.send((load1_, sum_arr))
         REGISTER_C.send((load2_, sum_arr))
         PC.send((load3_, sum_arr))
-        if count == 3:
+        if count == 200:
             break
         count += 1
 
